435e6cd84bf1125650424a777eb880e0dfe91c2a
yash <yash.khandelwal@aurigait.com>
2022-12-25 22:37:36 +0530

    Creating Spring Boot Application

---
+++ src/main/java/KickStart.java
@@ -0,0 +1,9 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KickStart {
+    public static void main(String[] args) {
+        SpringApplication.run(KickStart.class, args);
+    }
+}
